768442d8806951768e971e61ca8fef89dc987396
dotPast <83116565+dotPast@users.noreply.github.com>
2024-02-25 21:04:15 +0300

    main: set default scale to 100%

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-customtkinter.set_widget_scaling(0.8)
+customtkinter.set_widget_scaling(1)
 
 class App(customtkinter.CTk):
     def __init__(self):
