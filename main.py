622674ccb4949b9fcdbd52ee0430c37fcae7c610
VayLorm <arsenbelokurov906@mail.com>
2024-02-25 18:28:33 +0300

    heelo

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 import dearpygui.dearpygui as dpg
 import pygame
-import time
-
-class UserInputData:
-    def __init__(self):
-        self.first_input_text = ""
-        self.second_input_text = ""
 
 def close_viewport(sender, app_data):
     dpg.stop_dearpygui()
 
-def get_user_inputs(sender, app_data):
-    inputs_data = app_data["inputs"]
-    first_input_text = inputs_data.first_input_text
-    second_input_text = inputs_data.second_input_text
-    
-    # Вывод результата или любые другие действия с текстом
-    print(f'First input text: {first_input_text}')
-    print(f'Second input text: {second_input_text}')
-
-def update_app_data(sender, app_data):
-    inputs_data = app_data['inputs']
-    new_input_values = {}
-
-    for child_id in sender.children:
-        control_type = dpg.get_value_converted(child_id, dpg.mvItemType.MV_ITEM_TYPE_USER_DEFINED)
-        if control_type == 'input_text':
-            value = dpg.get_value(child_id)
-            new_input_values[control_type + str(child_id)] = value
-
-    inputs_data.update(new_input_values)
+def get_input_text(sender, app_data, user_data):
+    text = dpg.get_value(sender)
+    print(f"text: {text}\nsender: {sender}\napp_data: {user_data}")
 
 pygame.init()
 size = pygame.display.get_desktop_sizes()[0]
@@ -44,17 +21,14 @@ with dpg.font_registry():
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
 
-app_data = {"inputs": UserInputData()}
+with dpg.window(label="Tutorial"):
+    dpg.add_checkbox(label="Radio Button1", source="bool_value")
+    dpg.add_checkbox(label="Radio Button2", source="bool_value")
 
-with dpg.window(label="Example Window", height=500, width=800, min_size=(500, 300)):
-    dpg.add_text("Enter two strings:")
-    first_input_id = dpg.add_input_text(label="String 1", default_value="Quick brown fox")
-    second_input_id = dpg.add_input_text(label="String 2", default_value="Lazy dog")
-    dpg.add_button(label="Show Inputs", callback=get_user_inputs, user_data=app_data)
-    dpg.bind_item_user_data(first_input_id, app_data, "inputs.first_input_text")
-    dpg.bind_item_user_data(second_input_id, app_data, "inputs.second_input_text")
-    dpg.set_item_callback(first_input_id, update_app_data)
-    dpg.set_item_callback(second_input_id, update_app_data)
+    dpg.add_input_text(label="Text Input 1", source="string_value")
+    dpg.add_input_text(label="Text Input 2", source="string_value", password=True)
+    
+    dpg.add_button(label="Close", callback=close_viewport)
 
 dpg.create_viewport(title='MXmanager', 
                     width=width, 
